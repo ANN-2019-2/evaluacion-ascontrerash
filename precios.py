4fad57ead8225941c07624b1aab9f2a959ebd374
Alex <ascontrerash@unal.edu.co>
2020-03-26 00:37:42 -0500

    previo a entrenamiento precios

--- precios.py
+++ precios.py
@@ -31,3 +31,20 @@ data = data[data.columns[:-2]]
 data = data.fillna(0)
 data['promedio'] = data.mean(axis=1)
 data = data.reset_index(drop=True)
+data = data[['Fecha','promedio']]
+
+
+ipc = pd.read_excel('datos/1.2.5.IPC_Serie_variaciones.xlsx',header =12)
+ipc = ipc.iloc[:-6] 
+ipc = ipc.iloc[:,0:2] 
+ipc.columns = ['Corte', 'ipc']
+ipc['Corte'] = ipc['Corte'].astype(str)
+ipc['Corte'] = ipc['Corte'].apply(lambda x: x[:4] + '-' + x[4:])
+ipc['Corte'] = pd.to_datetime(ipc['Corte']).dt.to_period('M')
+indiceBase = ipc.tail(1).iloc[0,1]
+data['Corte'] = pd.to_datetime(data['Fecha']).dt.to_period('M')
+
+data = pd.merge(data, ipc[["Corte", "ipc"]], on="Corte", how="left")
+data['promedioIPC'] =  data['promedio'] * (indiceBase / data['ipc'])
+
+
